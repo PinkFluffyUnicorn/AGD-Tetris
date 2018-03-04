b7e4bc9b8f8360033f303b06ac8c9d822cf931c4
Sara Zecha <sara.zecha@unitybox.de>
2018-03-04 11:05:08 +0100

    pictures for daily Challenges
    also DailyChallenge and DailychallengeControllerScript, but without any function yet

--- UnityProjekt/Match3/Assets/Event1Script.cs
+++ UnityProjekt/Match3/Assets/Event1Script.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Event1Script : MonoBehaviour {
 
-    
+    private int durationOfEvent = 30; //minutes
 
     public void OnEnable()
     {
         //Zeit die schon am Tag rum ist in Minuten um keine Rechenfehler zu haben 
         double startTime = System.DateTime.Now.TimeOfDay.TotalMinutes;
+        double endTime = startTime + durationOfEvent;
 
     }
 
--- UnityProjekt/Match3/Assets/EventController.cs
+++ UnityProjekt/Match3/Assets/EventController.cs
@@ -11,6 +11,7 @@ public class EventController : MonoBehaviour {
     public GameObject AdvertiseQuestionaire;
     public GameObject Questionaire;
 
+
     // Use this for initialization
     void Start ()
     {
@@ -21,29 +22,35 @@ public class EventController : MonoBehaviour {
         }
 
 		//abfragen ob heute der Tag ist um das Event anzuzeigen 
-        if(CalculateDaysSinceStart() == 1)
+        if(CalculateDaysSinceStart() == 1 && PlayerPrefs.GetInt("AdvertiseEvent1", 0) == 0)
         {
             AdvertiseEvent1.SetActive(true);
+            PlayerPrefs.SetInt("AdvertiseEvent1", 1);
         }
-        if(CalculateDaysSinceStart() == 2)
+        if(CalculateDaysSinceStart() == 2 && PlayerPrefs.GetInt("Event1", 0) == 0)
         {
             Event1.SetActive(true);
+            PlayerPrefs.SetInt("Event1", 1);
         }
-        if (CalculateDaysSinceStart() == 3)
+        if (CalculateDaysSinceStart() == 3 && PlayerPrefs.GetInt("AdvertiseEvent2", 0) == 0)
         {
             AdvertiseEvent2.SetActive(true);
+            PlayerPrefs.SetInt("AdvertiseEvent2", 1);
         }
-        if (CalculateDaysSinceStart() == 4)
+        if (CalculateDaysSinceStart() == 4 && PlayerPrefs.GetInt("Event2", 0) == 0)
         {
             Event2.SetActive(true);
+            PlayerPrefs.SetInt("Event2", 1);
         }
-        if (CalculateDaysSinceStart() == 6)
+        if (CalculateDaysSinceStart() == 6 && PlayerPrefs.GetInt("AdvQuestionaire", 0) == 0)
         {
             AdvertiseQuestionaire.SetActive(true);
+            PlayerPrefs.SetInt("AdvQuestionaire", 1);
         }
-        if (CalculateDaysSinceStart() == 7)
+        if (CalculateDaysSinceStart() == 7 && PlayerPrefs.GetInt("Questionaire", 0) == 0)
         {
             Questionaire.SetActive(true);
+            PlayerPrefs.SetInt("Questionaire", 1);
         }
     }
 
