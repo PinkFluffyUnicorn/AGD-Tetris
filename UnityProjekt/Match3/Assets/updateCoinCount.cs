2c875ed32fba38f04f9a44d4f28dd9cd52fbcd14
Tobias Benecke <tobias.benecke@st.ovgu.de>
2018-03-07 00:57:13 +0100

    forgot to add a script

---
+++ UnityProjekt/Match3/Assets/updateCoinCount.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class updateCoinCount : MonoBehaviour {
+
+    public GameObject moneyHandler;
+    public Text moneyCount;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moneyCount.text = moneyHandler.GetComponent<MoneyHandlerScript>().getMoney().ToString();
+    }
+}
