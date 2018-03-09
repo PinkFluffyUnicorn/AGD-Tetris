ec2cc41ac9b2ccf7939c7c08a1cb58e9161b9bb1
Sara Zecha <sara.zecha@unitybox.de>
2018-03-09 13:52:45 +0100

    started 1. Event

---
+++ UnityProjekt/Match3/Assets/Event1Script_topLeft.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event1Script_topLeft : MonoBehaviour {
+
+    public GameObject Panel;
+    public GameObject Background;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		if(PlayerPrefs.GetInt("Event1GoingOn", 0) == 1)
+        {
+            Panel.SetActive(true);
+            Background.SetActive(true);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (PlayerPrefs.GetInt("Event1GoingOn", 0) == 1)
+        {
+            Panel.SetActive(true);
+            Background.SetActive(true);
+        }
+    }
+}
