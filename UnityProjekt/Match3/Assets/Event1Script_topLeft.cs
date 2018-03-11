1d1f9b47b7a677562336f1851b5dbf64a67c7a32
Sara Zecha <sara.zecha@unitybox.de>
2018-03-11 19:11:30 +0100

    fixed typo

--- UnityProjekt/Match3/Assets/Event1Script_topLeft.cs
+++ UnityProjekt/Match3/Assets/Event1Script_topLeft.cs
@@ -80,7 +80,7 @@ public class Event1Script_topLeft : MonoBehaviour {
             int DurationEvent1 = EventHandler.GetComponent<EventHandlerScript>().getDurationEvent1();
             Panel.SetActive(true);
             Background.SetActive(true);
-            int time = DurationEvent1 - ((int)System.DateTime.Now.TimeOfDay.TotalSeconds - StartEvent1))
+            int time = DurationEvent1 - ((int)System.DateTime.Now.TimeOfDay.TotalSeconds - StartEvent1);
             float hoursHelp = ((time / 60) / 60);
             int hours = Mathf.FloorToInt(hoursHelp);
             int seconds = time % 60;
