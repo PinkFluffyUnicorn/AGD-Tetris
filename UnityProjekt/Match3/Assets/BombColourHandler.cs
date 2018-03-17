7bb94c64c246e21539a0057d478cb190cac8ed12
Sara Zecha <sara.zecha@unitybox.de>
2018-03-17 10:23:50 +0100

    tried to add Script to change colout of bomb: not successful yet

---
+++ UnityProjekt/Match3/Assets/BombColourHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombColourHandler : MonoBehaviour {
+
+    public Sprite BombBlue;
+    public Sprite BombGreen;
+    public Sprite BombRed;
+    public Sprite BombPurple;
+    public Sprite BombOrange;
+    public Sprite finalBomb;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+}
