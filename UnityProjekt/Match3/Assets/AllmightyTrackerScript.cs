a14ad233a1e712d2fc7339e0935b33d3b5cd4160
Tobias Benecke <tobias.benecke@st.ovgu.de>
2018-03-24 14:51:50 +0100

    added the AllmightyTracker to track every move of the user

---
+++ UnityProjekt/Match3/Assets/AllmightyTrackerScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AllmightyTrackerScript : MonoBehaviour {
+
+    //the pat for match.me on android is "Dieser PC\Galaxy S6\Phone\Android\obb"
+
+
+    public static AllmightyTrackerScript allmightyTracker;
+	// Use this for initialization
+	void Awake () {
+        if(allmightyTracker == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            allmightyTracker = this;
+        }
+        else if(allmightyTracker != this)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnApplicationFocus(bool inFocus)
+    {
+        if (inFocus)
+        {
+            //your app is in the background, yay!!!!
+            print(DateTime.Now.ToString("dd.MM.yyy HH:mm:ss") + " active");
+        }
+        else
+        {
+            //the game is active! more yay!
+            print(DateTime.Now.ToString("dd.MM.yyy HH:mm:ss") + " inactive");
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
