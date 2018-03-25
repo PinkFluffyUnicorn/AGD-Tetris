cdb9e6864e22d52ae5027e0339b9779bfc881df7
Sara Zecha <sara.zecha@unitybox.de>
2018-03-25 21:08:29 +0200

    vertical & horizontal bombs for Challenges fixed
    fixed bug in Daily Challenge Pop up

--- UnityProjekt/Match3/Assets/BombColourHandler.cs
+++ UnityProjekt/Match3/Assets/BombColourHandler.cs
@@ -8,29 +8,55 @@ public class BombColourHandler : MonoBehaviour
 
     public Sprite Bombcolour;
     bool bomb = false;
+    bool horizontalBomb = false;
+    bool verticalBomb = false;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-        BombAction[] allChildren = GetComponentsInChildren<BombAction>();
-      
-            if(allChildren.Length > 0 )
-            {
-                bomb = true;
-            }
+        //bomb
+        BombAction[] bombs = GetComponentsInChildren<BombAction>();
 
-        if(bomb)
+        if (bombs.Length > 0)
+        {
+            bomb = true;
+        }
+
+        if (bomb)
         {
             gameObject.GetComponent<SpriteRenderer>().sprite = Bombcolour;
         }
-	}
-	
-	// Update is called once per frame
-	void Update ()
+
+        //horizontal & vertical bomb set in update
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
         if (bomb)
         {
             gameObject.GetComponent<SpriteRenderer>().sprite = Bombcolour;
+        }
+
+        if (!horizontalBomb && !verticalBomb)
+        {
+            Transform[] transform = GetComponentsInChildren<Transform>();
+
+            foreach (Transform child in transform)
+            {
+                if (child.name.Contains("line"))
+                {
+                    var spriteName = gameObject.GetComponent<SpriteRenderer>().sprite.name;
+                    if (spriteName.Contains("_v_") && !verticalBomb)
+                    {
+                        verticalBomb = true;
+                    }
+                    if (spriteName.Contains("_h_") && !horizontalBomb)
+                    {
+                        horizontalBomb = true;
+                    }
+                }
+            }
         }
     }
 
@@ -39,6 +65,16 @@ public class BombColourHandler : MonoBehaviour
         if (bomb)
         {
             PlayerPrefs.SetInt("BombDestroyd", PlayerPrefs.GetInt("BombDestroyd", 0) + 1);
+        }
+
+        if(horizontalBomb)
+        {
+            PlayerPrefs.SetInt("HorizontalBombDestroyd", PlayerPrefs.GetInt("HorizontalBombDestroyd", 0) + 1);
+        }
+
+        if(verticalBomb)
+        {
+            PlayerPrefs.SetInt("VerticalBombDestroyd", PlayerPrefs.GetInt("VerticalBombDestroyd", 0) + 1);
         }
     }
 }
