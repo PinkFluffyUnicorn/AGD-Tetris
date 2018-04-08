6c3e2967eac8dec63f7d7629b87231226705722f
Sara Zecha <sara.zecha@unitybox.de>
2018-04-08 13:07:58 +0200

    continue in mainGame -> very hacky and must be changed

---
+++ UnityProjekt/Match3/Assets/OutOfMovesNew.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OutOfMovesNew : MonoBehaviour {
+
+    public Text amountString;
+    public MoneyHandlerScript moneyHandler;
+    public GameObject NotEnoughMoney;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClick()
+    {
+        int amount = int.Parse(amountString.text);
+        bool enoughMoney = moneyHandler.GetComponent<MoneyHandlerScript>().subtractMoney(amount);
+        if(!enoughMoney)
+        {
+            NotEnoughMoney.SetActive(true);
+        }
+        else
+        {
+            NotEnoughMoney.SetActive(false);
+        }
+    }
+}
