6098dfe078c16c47c0e596b0d323ecc56d8d64a3
Sara Zecha <sara.zecha@unitybox.de>
2018-04-12 21:09:56 +0200

    polished mainGame

---
+++ UnityProjekt/Match3/Assets/BonusMoney.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BonusMoney : MonoBehaviour
+{
+    public MoneyHandlerScript moneyHandler;
+    public int amount;
+    public GameObject NotEnoughMoney;
+    public Text text;
+	// Use this for initialization
+	void Start ()
+    {
+        text.text = moneyHandler.GetComponent<MoneyHandlerScript>().getMoney().ToString();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        text.text = moneyHandler.GetComponent<MoneyHandlerScript>().getMoney().ToString();
+	}
+
+    public void OnClick()
+    {
+        var successfull = moneyHandler.GetComponent<MoneyHandlerScript>().subtractMoney(amount); 
+        if(!successfull)
+        {
+            NotEnoughMoney.SetActive(true);
+        }
+        else
+        {
+            NotEnoughMoney.SetActive(false);
+        }
+    }
+}
