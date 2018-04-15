47a260de7f0ebc9592b23edc8bc7376f46802408
Tobias Benecke <tobias.benecke@st.ovgu.de>
2018-04-15 15:16:12 +0200

    fixed the highscore table

--- UnityProjekt/Match3/Assets/fillHighscoreOverview.cs
+++ UnityProjekt/Match3/Assets/fillHighscoreOverview.cs
@@ -17,6 +17,17 @@ public class fillHighscoreOverview : MonoBehaviour {
     public Text person9;
     public Text person10;
 
+    public Text levelNumber1;
+    public Text levelNumber2;
+    public Text levelNumber3;
+    public Text levelNumber4;
+    public Text levelNumber5;
+    public Text levelNumber6;
+    public Text levelNumber7;
+    public Text levelNumber8;
+    public Text levelNumber9;
+    public Text levelNumber10;
+
     int enterysInTable = 10;
 
     public Text buttonText;//TODO: set here
@@ -54,52 +65,76 @@ public class fillHighscoreOverview : MonoBehaviour {
     void fillTable(List<string> data, int startvalue)
     {
         if (data.Count >= startvalue + 1)
+        {
             person1.text = data[startvalue + 0];
+            levelNumber1.text = startvalue+1+"";
+        }
         else
             person1.text = "---";
 
-        if (data.Count >= startvalue + 2)
+        if (data.Count >= startvalue + 2) { 
             person2.text = data[startvalue + 1];
+            levelNumber2.text = startvalue+2 + "";
+        }
         else
             person2.text = "---";
 
-        if (data.Count >= startvalue + 3)
+        if (data.Count >= startvalue + 3) { 
             person3.text = data[startvalue + 2];
+            levelNumber3.text = startvalue + 3 + "";
+        }
         else
             person3.text = "---";
 
         if (data.Count >= startvalue + 4)
+        {
             person4.text = data[startvalue + 3];
+            levelNumber4.text = startvalue + 4 + "";
+        }
         else
             person4.text = "---";
 
         if (data.Count >= startvalue + 5)
+        {
             person5.text = data[startvalue + 4];
+            levelNumber5.text = startvalue + 5 + "";
+        }
         else
             person5.text = "---";
 
-        if (data.Count >= startvalue + 6)
+        if (data.Count >= startvalue + 6) { 
             person6.text = data[startvalue + 5];
+            levelNumber6.text = startvalue + 6 + "";
+        }
         else
             person6.text = "---";
 
-        if (data.Count >= startvalue + 7)
+        if (data.Count >= startvalue + 7) { 
             person7.text = data[startvalue + 6];
+            levelNumber7.text = startvalue + 7 + "";
+        }
         else
             person7.text = "---";
 
         if (data.Count >= startvalue + 8)
+        {
             person8.text = data[startvalue + 7];
+            levelNumber8.text = startvalue + 8 + "";
+        }
         else
             person8.text = "---";
 
-        if (data.Count >= startvalue + 9)
+        if (data.Count >= startvalue + 9) { 
             person9.text = data[startvalue + 8];
+            levelNumber9.text = startvalue + 9 + "";
+        }
         else
             person9.text = "---";
 
-        if (data.Count >= startvalue + 10)
+        if (data.Count >= startvalue + 10) { 
             person10.text = data[startvalue + 9];
+            levelNumber10.text = startvalue + 10 + "";
+        }
         else
             person10.text = "---";
     }
